034fc30c8059b9236b73895f55784b2d6000eaca
white <abhijithkkpurameri@gmail.com>
2024-03-18 12:21:34 +0530

    routes setted in app.js page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
+import { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import FirstPage from "./Components/FirstPage/FirstPage";
 
 function App() {
- 
-  return (
-    <>
-      
-    </>
-  )
-}
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
 
-export default App
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag: HTMLMetaElement | null = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<FirstPage />} />
+    </Routes>
+  );
+}
+export default App;
